bac3d9470e27402d091c6d37d033ad4595d0dc9c
Takashi Ogura <t.ogura@gmail.com>
2012-06-09 22:24:48 +0900

    use TwoIntsRequest/Response

--- rosruby_tutorials/bin/add_two_ints_client.rb
+++ rosruby_tutorials/bin/add_two_ints_client.rb
@@ -14,8 +14,8 @@ node = ROS::Node.new('/rosruby/sample_service_client')
 
 if node.wait_for_service('/add_two_ints', 1)
   service = node.service('/add_two_ints', Roscpp_tutorials::TwoInts)
-  req = Roscpp_tutorials::TwoInts.request_class.new
-  res = Roscpp_tutorials::TwoInts.response_class.new
+  req = Roscpp_tutorials::TwoIntsRequest.new
+  res = Roscpp_tutorials::TwoIntsResponse.new
   req.a = ARGV[0].to_i
   req.b = ARGV[1].to_i
   if service.call(req, res)
